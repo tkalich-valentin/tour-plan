c9212fa935d0f699d2d6d42003e70444b608b92c
tkalich-valentin <tkalich-valentin@mail.ru>
2021-08-07 00:33:17 +0300

    https://github.com/tkalich-valentin/tour-plan.git

--- js/main.js
+++ js/main.js
@@ -1,20 +1,32 @@
-const swiper = new Swiper('.swiper-container', {
+var hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
 
   loop: true,
 
   // Navigation arrows
   navigation: {
-    nextEl: '.slider-button--next',
-    prevEl: '.slider-button--prev',
+    nextEl: '.hotel-slider__button--next',
+    prevEl: '.hotel-slider__button--prev',
   },
   keyboard: {
     enabled: true,
     onlyInViewport: false,
   },
+});
 
+const reviewsSlaider = new Swiper('.reviews-slider', {
+  // Optional parameters
 
+  loop: true,
 
-
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
 });
 
